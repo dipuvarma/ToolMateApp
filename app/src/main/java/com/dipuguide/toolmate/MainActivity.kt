0a9796b460b2c01fe97aea6eeb39776da8658232
Dipuvarma <dipuverma03072003@gmail.com>
2025-07-31 11:25:27 +0530

    Intital Commit

---
+++ app/src/main/java/com/dipuguide/toolmate/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.dipuguide.toolmate
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.dipuguide.toolmate.ui.theme.ToolMateTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ToolMateTheme(dynamicColor = false, darkTheme = false) {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Text(
+                        text = "Jai Shri Ram", modifier = Modifier
+                            .padding(innerPadding)
+                            .background(color = MaterialTheme.colorScheme.primaryContainer)
+                    )
+                }
+            }
+        }
+    }
+}
